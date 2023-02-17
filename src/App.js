909dc78d28e603b297f15d1ed5beb9508876eee3
Leonardo Hervert <leohervert@gmail.com>
2023-02-16 20:09:17 -0600

    [add] styles folder [update] Global Styles location [add] animation fade in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { GlobalStyle } from './styles/GlobalStyles'
+
 export const App = () => (
   <>
     <GlobalStyle />
