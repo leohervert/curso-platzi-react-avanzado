08d5f3bdc5126212d39950e76907c628c230694c
Leonardo Hervert <leohervert@gmail.com>
2023-02-16 19:59:10 -0600

    [add] Logo component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
