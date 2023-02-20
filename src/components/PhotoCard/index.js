e400afb7cf706ff228628f663d09e3caf635db61
Leonardo Hervert <leohervert@gmail.com>
2023-02-20 12:45:57 -0600

    [add] useNearScreen hook [add] lazy loading for not visible elements [add] useLocalStorage hook [add] Intersector Observer available for every navigator

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,36 @@
 import React from 'react'
-import { Button, ImgWrapper, Img } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
-const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+import { Button, ImgWrapper, Img, Article } from './styles'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from './useNearScreen'
+const DEFAULT_IMG =
+  'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
+  const key = `like-${id}`
+  const [show, ref] = useNearScreen()
+  const [liked, setLiked] = useLocalStorage(key, false)
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button
+            onClick={() => {
+              setLiked(!liked)
+            }}
+          >
+            <Icon size='32px' /> {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
