0060425d11853c16e40ff708083a1b44899259b0
Leonardo Hervert <leohervert@gmail.com>
2023-02-20 00:45:34 -0600

    add fixed bar with useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import db from '../../../api/db.json'
+
 export const ListOfCategories = () => {
-  return (
-    <List>
-      {db.categories.map(category => (
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+  useEffect(() => {
+    window
+      .fetch('https://petgran-server-lhervert-leohervert.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const isShowFixed = (window.scrollY > 160)
+      setShowFixed(isShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = fixed => (
+    <List className={fixed ? 'fixed' : ''}>
+      {categories.map(category => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/styles/animation.js
+++ src/styles/animation.js
@@ -1,18 +1,32 @@
 import { css, keyframes } from 'styled-components'
 
-export const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
+export const fadeIn = ({ time = '0.5s', type = 'ease' } = {}) =>
   css`
-    animation: ${time} 1s ${fadeInKeyFrames} ${type};
+    animation: ${time} ${fadeInKeyFrames} ${type};
   `
 
 const fadeInKeyFrames = keyframes`
   from {
-    filter: blur(5px);
+    filter: blur(3px);
     opacity: 0;
   }
 
   to {
     filter: blur(0);
     opacity: 1;
+  }
+`
+
+export const dropDown = ({ time = '0.5s', type = 'ease' } = {}) => css`
+  animation: ${time} ${dropDownKeyFrame} ${type};
+`
+
+const dropDownKeyFrame = keyframes`
+  from{
+    top: -70px;
+  }
+
+  to {
+    top: -20px;
   }
 `
