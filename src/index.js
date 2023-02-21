7018adc32bd5d34bcd8bdd27f31bb59b57284a41
Leonardo Hervert <leohervert@gmail.com>
2023-02-21 16:34:01 -0600

    [add] Apollo Provider with the default client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgran-server-lhervert-leohervert.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
