b69fd6272906fe9f91277d9345907f0a72e6cbd0
Leonardo Hervert <leohervert@gmail.com>
2023-02-22 23:22:12 -0600

    [add] Navbar to the App.js [add] animation fadein to navbar [update] change Link to NavLink to use aria-current

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Route, Routes } from 'react-router-dom'
 import { Detail } from './pages/Detail'
+import { NavBar } from './components/NavBar'
+
 export const App = () => {
   return (
     <>
@@ -14,7 +16,7 @@ export const App = () => {
         <Route path='/pet/:id' element={<Home />} />
         <Route path='/detail/:detailId' element={<Detail />} />
       </Routes>
-
+      <NavBar />
     </>
   )
 }
