0b9cdffe1a47d9783f53b6d28d435335bee22d50
Leonardo Hervert <leohervert@gmail.com>
2023-02-23 01:31:23 -0600

    [add] User Page [add] Favs Page [add] Not Register User Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Route, Routes } from 'react-router-dom'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
+import { User } from './pages/User'
+import { Favs } from './pages/Favs'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -15,7 +21,24 @@ export const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/pet/:id' element={<Home />} />
         <Route path='/detail/:detailId' element={<Detail />} />
+        {/* <Route path='/' element={<NotRegisteredUser />} /> */}
       </Routes>
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth
+            ? (
+              <Routes>
+                <Route path='/favs' element={<Favs />} />
+                <Route path='/user' element={<User />} />
+              </Routes>
+              )
+            : (
+              <Routes>
+                <Route path='/favs' element={<NotRegisteredUser />} />
+                <Route path='/user' element={<NotRegisteredUser />} />
+              </Routes>
+              )}
+      </UserLogged>
       <NavBar />
     </>
   )
