a594ba28818afaafccd7cda9889afef5bfe62926
Leonardo Hervert <leohervert@gmail.com>
2023-02-24 13:19:49 -0600

    [add] styles to UserForm [add] Register and Login Form

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
+import { Button, Container, Form, Input, Title } from './styles'
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
+
   return (
-    <form onSubmit={onSubmit}>
-      <input placeholder='Email' {...email} />
-      <input placeholder='Password' type='password' {...password} />
-      <button> Iniciar Sesion</button>
-    </form>
+    <Container>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}>
+        <Input placeholder='Email' {...email} />
+        <Input placeholder='Password' type='password' {...password} />
+        <Button>{title}</Button>
+      </Form>
+    </Container>
   )
 }
