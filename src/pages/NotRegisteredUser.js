16770ac2b0b501464f5e104d2a3dee3ca871815d
Leonardo Hervert <leohervert@gmail.com>
2023-02-24 15:15:29 -0600

    [add] mutation for register

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { UserForm } from '../components/UserForm'
 import Context from '../Context'
+import { RegisterMutation } from '../container/RegisterMutation'
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
         <>
-          <UserForm title='Registrarse' onSubmit={activateAuth} />
+          <RegisterMutation>
+            {register => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password }
+                const variables = { input }
+                register({ variables }).then(activateAuth)
+              }
+              return <UserForm title='Registrarse' onSubmit={onSubmit} />
+            }}
+          </RegisterMutation>
           <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
         </>
       )
