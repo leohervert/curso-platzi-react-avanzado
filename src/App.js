6b00ba6320de88753e89473032be4a25aeb3ff4f
Leonardo Hervert <leohervert@gmail.com>
2023-02-27 11:31:38 -0600

    [update] main router [update] redirect to login or home page [add] 404 page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Home } from './pages/Home'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
 import { User } from './pages/User'
 import { Favs } from './pages/Favs'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import Context from './Context'
+import { Context } from './Context'
 import { SignUp } from './pages/Signup'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route path='/' element={<Home />} />
         <Route path='/pet/:id' element={<Home />} />
         <Route path='/detail/:detailId' element={<Detail />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route
+          path='/login'
+          element={isAuth ? <Navigate to='/' /> : <NotRegisteredUser />}
+        />
+        <Route
+          exact
+          path='/favs'
+          element={isAuth ? <Favs /> : <Navigate replace to='/login' />}
+        />
+        <Route
+          exact
+          path='/user'
+          element={isAuth ? <User /> : <Navigate replace to='/login' />}
+        />
       </Routes>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth
-            ? (
-              <Routes>
-                <Route path='/favs' element={<Favs />} />
-                <Route path='/user' element={<User />} />
-              </Routes>
-              )
-            : (
-              <Routes>
-                <Route path='/favs' element={<NotRegisteredUser />} />
-                <Route path='/user' element={<NotRegisteredUser />} />
-                <Route path='/signup' element={<SignUp />} />
-              </Routes>
-              )}
-      </Context.Consumer>
       <NavBar />
     </>
   )
