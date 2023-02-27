123c4afa080a624bccb3ba7da83d7de3915d4099
Leonardo Hervert <leohervert@gmail.com>
2023-02-27 09:22:39 -0600

    [add] auth middleware (header authorization) [add] error middleware (close session when the token expires)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  from,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache
+} from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
 import Context from './Context'
+import { onError } from '@apollo/client/link/error'
+
+const httpLink = new HttpLink({
+  uri: 'https://petgran-server-lhervert-leohervert.vercel.app/graphql'
+})
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem('token')
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  }
+  return forward(operation)
+})
+
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === 'invalid_token') {
+    window.sessionStorage.removeItem('token')
+    window.location('/')
+  }
+})
 
 const client = new ApolloClient({
-  uri: 'https://petgran-server-lhervert-leohervert.vercel.app/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: from([errorMiddleware, authMiddleware, httpLink])
 })
 
 ReactDOM.render(
